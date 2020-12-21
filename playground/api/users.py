759339f6cca54a6a1f7192c20478efb950a5aa04
Peter Jones <pjones@devalot.com>
2020-12-21 10:42:07 -0700

    Add a way to fetch an address

--- playground/api/users.py
+++ playground/api/users.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, abort
 from playground.database import db_session, init_db
+from playground.models.address import Address
 from playground.models.user import User
 
 users_api = Blueprint("users_api", __name__)
@@ -25,3 +26,13 @@ def user_by_id(user_id):
 def mando():
     mando = User.find_by_name_and_address(db_session, "The Mandalorian", "Home")
     return jsonify(mando)
+
+
+@users_api.route("/users/<user_id>/addresses/<label>")
+def address_by_label(user_id, label):
+    address = Address.find_by_user_id_and_label(db_session, user_id, label)
+
+    if address is None:
+        abort(404)
+
+    return jsonify({"address": address, "user_name": address.user.name,})
--- playground/models/address.py
+++ playground/models/address.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from playground.database import Base
 from sqlalchemy import Column, DateTime, Integer, Text, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm.session import Session
 from sqlalchemy.sql import func
 
 
@@ -12,11 +13,15 @@ class Address(Base):
 
     # Columns that are automatically set by the database:
     id: int = Column(Integer, primary_key=True)
-    user_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
     created_at: datetime = Column(DateTime(timezone=True), default=func.now())
     updated_at: datetime = Column(
         DateTime(timezone=True), default=func.now(), onupdate=func.now()
     )
+
+    # Relationships:
+    user_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
+    # Can't use type hints here otherwise we get circular dependencies:
+    user = relationship("User", back_populates="addresses")
 
     # Other columns:
     label: str = Column(Text, nullable=False)
@@ -25,3 +30,15 @@ class Address(Base):
     def __init__(self, label, display_text):
         self.label = label
         self.display_text = display_text
+
+    @staticmethod
+    def find_by_user_id_and_label(db: Session, user_id: int, label: str):
+        from playground.models.user import User
+
+        return (
+            db.query(Address)
+            .filter(Address.label == label)
+            .join(User)
+            .filter(User.id == user_id)
+            .first()
+        )
